da5994ad27a999584f3a5109266673c1da9df95b
AbdelRahman Baba <abdelrahman@AbdelRahmans-MacBook-Pro.local>
2022-10-23 11:24:52 +0300

    first commit

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,9 +1,12 @@
-import { Menu } from 'antd'
+import { Avatar, Menu } from 'antd'
 import React from 'react'
 import './landing.scss'
+import Logo from '../../assets/logo.png'
 
 export default function landing() {
+  const logo = <Avatar src={Logo} size='64' style={{ minWidth: 80, minHeight: 80 }} />
   const items = [
+    { label: 'Kiddy', key: 'Kiddy', icon: logo },
     { label: 'About Us', key: 'About Us' },
     { label: 'Contact Us', key: 'Contact Us' },
     { label: 'Sign In', key: 'Sign In' }, // which is required
